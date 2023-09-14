1bd946937f09d03b7947d46b227ae930265fe9b8
mimunojun <mimura.tsukuba@gmail.com>
2023-09-15 02:42:08 +0900

    init

--- playground/modules.js
+++
@@ -1,9 +0,0 @@
-const { people, ages } = require('./people'); // importing 
-
-// console.log(xyz);
-console.log(people, ages);
-
-const os = require('os');
-
-console.log(os.platform(), os.homedir());
-
