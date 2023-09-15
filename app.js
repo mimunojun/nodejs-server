f29f58df31f3c348f07e77abc1c24eec8c605ad2
mimunojun <mimura.tsukuba@gmail.com>
2023-09-16 06:36:26 +0900

    view engine (ejs)

--- app.js
+++ app.js
@@ -3,25 +3,33 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs'); // app settings
+// app.set('views', 'myviews'); // if the name of html folder is not "views", you have to tell the name of that
+
 // listen for requests
 app.listen(3000); // port 3000
 
 app.get('/', (req, res) => {    // first arg: what link it listen to, second arg: what to do
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', { title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
 });
 
-// redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about'); // automatically sets status code (404)
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog' });
 });
 
 // 404
 app.use((req, res) => {     // it fires every single request
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404' }); // automatically sets status code (404)
 });
