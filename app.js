276ff5810a7025bd2c2463e7976893c220dd34e9
mimunojun <mimura.tsukuba@gmail.com>
2023-09-26 02:58:11 +0900

    db connected and manipulated

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
+
+const dbURI = "mongodb+srv://mimuratsukuba:test0000@nodetuts.vdsblss.mongodb.net/node-tuts?retryWrites=true&w=majority";
+mongoose.connect(dbURI)
+    .then((result) => console.log('connected to db'))
+    .then(() => app.listen(3000))
+    .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs'); // app settings
 // app.set('views', 'myviews'); // if the name of html folder is not "views", you have to tell the name of that
 
-// listen for requests
-app.listen(3000); // port 3000
-
 // middleware & static files
 app.use(express.static('public')); // inside the folder called public will be available; they could be accessed without stating like "/public/styles.css", but just "/styles.css"
 app.use(morgan('dev'));
 
+// routes
 app.get('/', (req, res) => {    // first arg: what link it listen to, second arg: what to do
-    // res.send('<p>home page</p>');
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
-    res.render('index', { title: 'Home', blogs});
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>about page</p>');
     res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 });
 
 app.get('/blogs/create', (req, res) => {
